f48a608ca7bbff6751c3dc76d85594302117b6b1
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-18 22:56:41 +0000

    working

--- app.js
+++ app.js
@@ -1,115 +1,104 @@
 var app = function(){
 
-  var rollBtn = document.getElementById('roll-dice')
-  var counter = 0;
-  rollBtn.addEventListener('click', function(){
-    if (counter < 6) {
-      showDiceRollNumber();
-      counter++;
-    }
-  })
+  // var rollBtn = document.getElementById('roll-dice')
+  // var counter = 0;
+  // rollBtn.addEventListener('click', function(){
+  //   if (counter < 6) {
+  //     showDiceRollNumber();
+  //     counter++;
+  //   }
+  // })
+
+  var btn = document.getElementById('reelroll');
+  btn.addEventListener('click', function(){
+    diceRoll();
+  });
 }
 
-var showDiceRollNumber = function(){
-  var results1 = document.getElementById('results1');
-  var dice = new Dice();
-  var diceRoll = dice.rollDice()
-  var resultContainer = document.createElement('div')
-  resultContainer.className = "flexer";
-  for(var number of diceRoll) {
-    var result = document.createElement('p');
-    result.innerText = number;
-    resultContainer.appendChild(result)
-    results1.appendChild(resultContainer);
+var diceRoll = function() {
+  var arr = [1, 2, 3, 4, 5, 6];
+  if(document.getElementById("hold1").checked == false){
+    document.getElementById("reel1").innerHTML = arr[Math.floor(Math.random() * arr.length)];
   }
+  if(document.getElementById("hold2").checked == false){
+    document.getElementById("reel2").innerHTML = arr[Math.floor(Math.random() * arr.length)];
+  }
+  if(document.getElementById("hold3").checked == false){
+    document.getElementById("reel3").innerHTML = arr[Math.floor(Math.random() * arr.length)];
+  }
+  if(document.getElementById("hold4").checked == false){
+    document.getElementById("reel4").innerHTML = arr[Math.floor(Math.random() * arr.length)];
+  }
+  if(document.getElementById("hold5").checked == false){
+    document.getElementById("reel5").innerHTML = arr[Math.floor(Math.random() * arr.length)];
+  }
+
+
 }
-
-var Dice = function(){
-<<<<<<< HEAD
-  var die = new Die();
-  this.dice = [die, die, die, die, die];
-  this.shipExist = false;
-  this.captExist = false;
-  this.crewExist = false;
-  this.cargo = 0;
-  this.numOfRolls = 0;
-};
-
-Dice.prototype.rollDice = function() {
-  var gameArray = [];
-  var diceResults = [];
-  for(die of this.dice) {
-    var result = die.roll();
-    if (result === 6 && (this.shipExist !== true)) {
-      gameArray.push(result);
-      this.shipExist = true;
-      this.dice.splice(result, 1);
-    } else if (result === 5 && (this.shipExist) && (this.captExist !== true)) {
-      gameArray.push(result);
-      this.captExist = true;
-      this.dice.splice(result, 1);
-
-    } else if (result === 4 && (this.crewExist !== true) && (this.captExist) && (this.shipExist)) {
-      gameArray.push(result);
-      this.crewExist = true;
-      this.dice.splice(result, 1);
-
-    } else {
-      diceResults.push(result);
-    }
-  }
-  console.log(gameArray);
-  // if (diceResults.length === 2) {
-  //   console.log(diceResults.reduce)
-  return diceResults.sort().reverse();
+//
+// var showDiceRollNumber = function(){
+//   var results1 = document.getElementById('results1');
+//   var dice = new Dice();
+//   var diceRoll = dice.rollDice()
+//   var resultContainer = document.createElement('div')
+//   resultContainer.className = "flexer";
+//   for(var number of diceRoll) {
+//     var result = document.createElement('p');
+//     result.innerText = number;
+//     resultContainer.appendChild(result)
+//     results1.appendChild(resultContainer);
+//   }
 // }
-=======
-    var die = new Die();
-    this.dice = [die, die, die, die, die];
-    this.shipExists = false;
-    this.captainExists = false;
-    this.crewExists = false;
-    this.cargo = 0;
-    this.numOfRolls = 0;
-};
-
-Dice.prototype.rollDice = function() {
-    var diceResults = [];
-    var winningDiceRoll = 0;
-    for(die of this.dice) {
-        var result = die.roll();
-        if (result === 6 && !shipExist){
-            shipExist =true;
-            winningDiceRoll += 1;
-        }else
-        if(result === 5 && shipExists && !captainExists){
-            captainExists = true;
-            winningDiceRoll +=1;
-        }else
-        if(result === 4 && shipExists && captainExists && !crewExists){
-            crewExists = true;
-            winningDiceRoll += 1;
-        }
-
-        .sort(function(a, b){return b - a});
-
-
-        diceResults.push(result);
-    };
-    return diceResults;
->>>>>>> c4cf9a3fcde1ba8a57235e7ee6b9c31c8925401e
-};
-
-
-var Die = function(){
-  this.sides = 6
-  this.banked = false;
-};
-
-Die.prototype.roll = function(){
-  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-  return randomNumber;
-}
-
-
+//
+// var Dice = function(){
+//
+//   this.dice = [];
+//   this.shipExist = false;
+//   this.captExist = false;
+//   this.crewExist = false;
+//   this.cargo = 0;
+//   this.numOfRolls = 0;
+// };
+//
+// Dice.prototype.rollDice = function() {
+//   var die = new Die();
+//   var myDice = [die, die, die, die, die];
+//
+//   var gameArray = [];
+//   var diceResults = [];
+//   var winningDice = 1;
+//   for(var i = 0; i < myDice.length; i++){
+//     var result = myDice[i].roll();
+//     if (result === 6 && (this.shipExist !== true)) {
+//       gameArray.push(result);
+//       this.shipExist = true;
+//       myDice.splice(0, winningDice)
+//     } else if (result === 5 && (this.shipExist) && (this.captExist !== true)) {
+//       gameArray.push(result);
+//       this.captExist = true;
+//       myDice.splice(0, winningDice)
+//     } else if (result === 4 && (this.crewExist !== true) && (this.captExist) && (this.shipExist)) {
+//       gameArray.push(result);
+//       this.crewExist = true;
+//       myDice.splice(0, winningDice)
+//     } else {
+//       diceResults.push(result);
+//     }
+//     console.log(myDice.length)
+//   }
+//   return diceResults.sort();
+// };
+//
+//
+// var Die = function(){
+//   this.sides = 6
+//   this.banked = false;
+// };
+//
+// Die.prototype.roll = function(){
+//   var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//   return randomNumber;
+// }
+//
+//
 window.addEventListener('load', app)

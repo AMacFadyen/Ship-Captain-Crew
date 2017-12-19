053551dbe40bfd00f42ff9ab1d0a57dbb3115375
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-19 10:03:55 +0000

    game logic done

--- app.js
+++ app.js
@@ -12,106 +12,87 @@ var app = function(){
   var btn = document.getElementById('reelroll');
   btn.addEventListener('click', function(){
     diceRoll();
+    computerDiceRoll();
   });
 }
 
 var diceRoll = function() {
   var arr = [1, 2, 3, 4, 5, 6];
-  if(document.getElementById("hold1").checked == false){
-    document.getElementById("reel1").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-  } else {
-    document.getElementById("hold1").disabled = true;
-  }
+  counter=1;
+  var resultsArray = [];
+  for (var i = 0; i < 5; i++) {
+    var isChecked = document.getElementById("hold"+counter).checked
+    if(isChecked == false){
+      var result = arr[Math.floor(Math.random() * arr.length)];
+      resultsArray.push(result);
+      resultsArray.sort();
+      counter++;
+    }
+    var index = 1;
 
-  if(document.getElementById("hold2").checked == false){
-    document.getElementById("reel2").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-  } else {
-    document.getElementById("hold2").disabled = true;
-  }
+    var six = document.getElementById("hold5").checked;
+    var five = document.getElementById("hold4").checked;
+    var four = document.getElementById("hold3").checked;
 
-  if(document.getElementById("hold3").checked == false){
-    document.getElementById("reel3").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-  } else {
-    document.getElementById("hold3").disabled = true;
-  }
 
-  if(document.getElementById("hold4").checked == false){
-    document.getElementById("reel4").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-  } else {
-    document.getElementById("hold4").disabled = true;
+    for (var number of resultsArray) {
+      var sortedResult = document.getElementById("reel"+index).innerText = number;
+      if (resultsArray[4] === 6 || (six) ) {
+        document.getElementById("hold5").checked = true;
+        if (resultsArray[3] === 5 || (five)) {
+          document.getElementById("hold4").checked = true;
+          if (resultsArray[2] === 4) {
+            document.getElementById("hold3").checked = true;
+            var btn = document.getElementById('reelroll');
+            btn.disabled = true;
+            var victory = document.getElementById('victory');
+            victory.innerText = "Player wins!"
+          }
+        }
+      }
+      index++;
+    }
   }
-
-  if(document.getElementById("hold5").checked == false){
-    document.getElementById("reel5").innerHTML = arr[Math.floor(Math.random() * arr.length)];
-  } else {
-    document.getElementById("hold5").disabled = true;
-  }
-
 }
-//
-// var showDiceRollNumber = function(){
-//   var results1 = document.getElementById('results1');
-//   var dice = new Dice();
-//   var diceRoll = dice.rollDice()
-//   var resultContainer = document.createElement('div')
-//   resultContainer.className = "flexer";
-//   for(var number of diceRoll) {
-//     var result = document.createElement('p');
-//     result.innerText = number;
-//     resultContainer.appendChild(result)
-//     results1.appendChild(resultContainer);
-//   }
-// }
-//
-// var Dice = function(){
-//
-//   this.dice = [];
-//   this.shipExist = false;
-//   this.captExist = false;
-//   this.crewExist = false;
-//   this.cargo = 0;
-//   this.numOfRolls = 0;
-// };
-//
-// Dice.prototype.rollDice = function() {
-//   var die = new Die();
-//   var myDice = [die, die, die, die, die];
-//
-//   var gameArray = [];
-//   var diceResults = [];
-//   var winningDice = 1;
-//   for(var i = 0; i < myDice.length; i++){
-//     var result = myDice[i].roll();
-//     if (result === 6 && (this.shipExist !== true)) {
-//       gameArray.push(result);
-//       this.shipExist = true;
-//       myDice.splice(0, winningDice)
-//     } else if (result === 5 && (this.shipExist) && (this.captExist !== true)) {
-//       gameArray.push(result);
-//       this.captExist = true;
-//       myDice.splice(0, winningDice)
-//     } else if (result === 4 && (this.crewExist !== true) && (this.captExist) && (this.shipExist)) {
-//       gameArray.push(result);
-//       this.crewExist = true;
-//       myDice.splice(0, winningDice)
-//     } else {
-//       diceResults.push(result);
-//     }
-//     console.log(myDice.length)
-//   }
-//   return diceResults.sort();
-// };
-//
-//
-// var Die = function(){
-//   this.sides = 6
-//   this.banked = false;
-// };
-//
-// Die.prototype.roll = function(){
-//   var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-//   return randomNumber;
-// }
-//
-//
+
+
+var computerDiceRoll = function() {
+  var arr = [1, 2, 3, 4, 5, 6];
+  counter=1;
+  var resultsArray = [];
+  for (var i = 0; i < 5; i++) {
+    var isChecked = document.getElementById("cpu-hold"+counter).checked
+    if(isChecked == false){
+      var result = arr[Math.floor(Math.random() * arr.length)];
+      resultsArray.push(result);
+      resultsArray.sort();
+      counter++;
+    }
+    var index = 1;
+
+    var six = document.getElementById("cpu-hold5").checked;
+    var five = document.getElementById("cpu-hold4").checked;
+    var four = document.getElementById("cpu-hold3").checked;
+
+    for (var number of resultsArray) {
+      var sortedResult = document.getElementById("cpu-reel"+index).innerText = number;
+      if (resultsArray[4] === 6 || (six) ) {
+        document.getElementById("cpu-hold5").checked = true;
+        if (resultsArray[3] === 5 || (five)) {
+          document.getElementById("cpu-hold4").checked = true;
+          if (resultsArray[2] === 4) {
+            document.getElementById("cpu-hold3").checked = true;
+            var btn = document.getElementById('reelroll');
+            btn.disabled = true;
+            var victory = document.getElementById('victory');
+            victory.innerText = "Computer wins!"
+
+          }
+        }
+      }
+      index++;
+    }
+  }
+}
+
 window.addEventListener('load', app)
